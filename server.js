aac0a98e093db83bff28dcc9ac51467c95b47980
travjbarnes <travjbarnes@gmail.com>
2017-08-27 14:10:42 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-
 var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json();
+//app.use(bodyParser.json());
 
-var urlencodedParser = bodyParser.urlencoded({extended:false})
-
-
-
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
+//app.use(bodyParser.raw({type: 'application/vnd.api+json' }));
+app.use(bodyParser.raw({type: 'application/vnd.custom-type' }));
+
+app.use(bodyParser.text({type: 'text/html'}))
 
 require("./app/routing/html-routes.js")(app);
 require("./app/routing/api-routes.js")(app);
